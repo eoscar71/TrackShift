b7b0404c642bccb0e4d9b0efbba53f086cf34464
Emmanuel <mannyayala7190@gmail.com>
2022-12-19 16:13:05 -0500

    Added toast notifications on invalid login/signup.

--- trackshift/src/App.js
+++ trackshift/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Routes, Route, Navigate } from "react-router";
+import { ToastContainer } from "react-toastify";
 import HomePage from "./components/homePage";
 import MigratePage from "./components/migratePage";
 import AuthRedirect from './components/authRedirect';
@@ -8,13 +9,14 @@ import WithoutNav from './components/common/withoutNav';
 import jwtDecode from "jwt-decode";
 import "./App.css";
 import "animate.css";
+import "react-toastify/dist/ReactToastify.css";
 
 class App extends Component {
   state = {};
 
   getCurrentUser() {
     try {
-      const jwt = localStorage.getItem('token');
+      const jwt = localStorage.getItem("token");
       return jwtDecode(jwt);
     } catch (error) {
       return null;
@@ -29,32 +31,38 @@ class App extends Component {
     console.log("user: ", user);
     return (
       <React.Fragment>
+        <ToastContainer />
         <Routes>
-            <Route element={<WithoutNav/>}>
+          <Route element={<WithoutNav />}>
             <Route
-                path="/auth-redirect"
-                element={
-                  user ? (
-                    <AuthRedirect urlParams={urlParams} />
-                  ) : (
-                    <Navigate to="/" />
-                  )
-                }
-              />
-            </Route>
-            <Route element={<WithNav user={user}/>}>
+              path="/auth-redirect"
+              element={
+                user ? (
+                  <AuthRedirect urlParams={urlParams} />
+                ) : (
+                  <Navigate to="/" />
+                )
+              }
+            />
+          </Route>
+          <Route element={<WithNav user={user} />}>
             <Route
-                path="/migrate"
-                element={
-                  user ? (
-                    <MigratePage urlParams={urlParams} />
-                  ) : (
-                    <Navigate to="/" />
-                  )
-                }
-              />
-              <Route path="/" element={<HomePage user={user}/>} />
-            </Route>
+              path="/migrate"
+              element={
+                user ? (
+                  <MigratePage urlParams={urlParams} />
+                ) : (
+                  <Navigate to="/" />
+                )
+              }
+            />
+            <Route
+              path="/"
+              element={
+                !user ? <HomePage user={user} /> : <Navigate to="/migrate" />
+              }
+            />
+          </Route>
         </Routes>
       </React.Fragment>
     );
--- trackshift/src/services/userService.js
+++ trackshift/src/services/userService.js
@@ -1,35 +1,76 @@
-import axios from "axios";
+import { toast } from "react-toastify";
+import http from '../services/httpService';
+import config from "../config.json";
 
-axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
 
 export async function changePassword(currentPassword, newPassword) {
-    const {data: response} = await axios.put('http://localhost:3900/api/users', {
-        currentPassword: currentPassword,
-        newPassword: newPassword
-    });
-    return response;
+    try {
+        const response = await http.put(config.apiUrl + '/users', {
+            currentPassword: currentPassword,
+            newPassword: newPassword
+        });
+        toast('Password change successful!');
+        return response;
+    } catch (error) {
+        if(error.response.status===400)
+            toast.error(error.response.data);
+    }
 }
 
 export async function deleteAccount() {
-    const {data: response} = await axios.delete('http://localhost:3900/api/users');
+    const {data: response} = await http.delete(config.apiUrl + '/users');
     return response;
 }
 
 export async function register(email, password) {
-    return axios.post('http://localhost:3900/api/users', {
-        email: email,
-        password: password
-    });
+    try {
+        const response = await http.post(config.apiUrl + '/users', {
+            email: email,
+            password: password
+        });
+        
+        localStorage.setItem("token", response.headers["x-auth-token"]);
+        setLocalStorageItems();
+        window.location = "/migrate";
+  
+        return response;
+  
+      } catch (error) {  
+        if(error.response.status===400)
+            toast.error(error.response.data);
+      }     
 }
 
 export async function login(email, password) {
-    return axios.post('http://localhost:3900/api/auth/users', {
-        email: email,
-        password: password
-    });
+    try {
+      const response = await http.post(
+        config.apiUrl + "/auth/users",
+        {
+          email: email,
+          password: password,
+        }
+      );
+      
+      localStorage.setItem("token", response.data);
+      setLocalStorageItems();
+      window.location = "/migrate";
+
+      return response;
+
+    } catch (error) {
+
+      if(error.response.status===400)
+          toast.error('Invalid username or password.');
+    }
 }
 
 export async function logout() {
     localStorage.clear();
     window.location = "/";
+}
+
+function setLocalStorageItems() {
+    localStorage.setItem("hasSpotifyAuth", false);
+    localStorage.setItem("hasYoutubeAuth", false);
+    localStorage.setItem("hasDeezerAuth", false);
 }
